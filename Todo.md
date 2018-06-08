7bec50506b56f42cb95f44b66a655ccaefd753bb
kai's mac <kill4410@163.com>
2018-06-08 13:21:37 +0800

    add random_sample python file

--- Todo.md
+++ Todo.md
@@ -2,14 +2,24 @@
 
 + 训练集有6G这么大，需要采样一小部分，来做小实验。
 
-   尝试以下几种做法：
+ 尝试以下几种做法：
 
-  ```shell
-  (head -n 500000 $train_path && tail -n 500000 $train_path) > $subsampled_data_path
-  ```
+```shell
+(head -n 500000 $train_path && tail -n 500000 $train_path) > $subsampled_data_path
+```
 
-  但是这个会包含开头。我们不想要第一行。
+但是这个会包含开头。我们不想要第一行。
 
-  
+使用以下这个
 
-  
+```shell
+tail -n +2 $train_path | head -n 500000 > $subsampled_data_path
+tail -n 500000 $train_path >> $subsampled_data_path
+```
+
+也可以使用随机数进行采样工作
+
+
+
++ 有了小的train以后，可以开始做一些尝试。可以从kaggle上的已有ftrl模型开始。
+
---
+++ scripts_for_data/random_subsample.py
@@ -0,0 +1,46 @@
+import random
+random.seed(42)
+import 	sys
+from datetime import datetime
+
+
+def random_subsample(path, sample_size = 10, has_header = False):
+
+	t1 =  datetime.now()
+	# first we calculate totally how many line are there in the input
+	with open(path, "r") as f:
+		for linecount, line in enumerate(f):
+			if linecount == 0 and has_header:
+				header = line
+	if has_header:
+		linecount -= 1	
+	t2 = datetime.now()
+	print("File has Header: {}, the header is {}".format(has_header,header))
+	print("Time for counting lines is {} .".format(t2 - t1 ))
+	print("Totally there are {} lines in the original file. ".format(linecount))
+
+
+	t1 =  datetime.now()
+
+	output_path = "/Users/kaishen/git_storage/avazuCTR/subsampled_data/random_subsample100m"
+	random_list = random.sample(range(linecount), sample_size)
+	random_list.sort()
+	if has_header:
+		random_list = [ x+1 for x in random_list]
+
+	with open(output_path, "w+") as fout:
+		with open(path,"r") as fin:
+			temp = 0;
+			for linecount_, line in enumerate(fin):
+				if linecount_ == random_list[temp]:
+					temp += 1
+					fout.write(line)
+				if temp >= sample_size:
+					break
+	t2 = datetime.now()
+	print("Time for generating subsampled_data is {} .".format(t2 - t1 ))
+
+
+
+if __name__ == "__main__":
+	random_subsample(sys.argv[1], int(sys.argv[2]), sys.argv[3])
